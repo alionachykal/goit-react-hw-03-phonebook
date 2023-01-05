d266e0ea210fc52fd9d75d223b6e89f7b8546495
alionachykal <106018567+alionachykal@users.noreply.github.com>
2023-01-05 16:32:52 +0300

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
-// import { ContactItem } from './ContactItem/ContactItem';
+
 
 export class App extends Component {
   state = {
